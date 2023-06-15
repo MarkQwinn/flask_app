ba498cac0d51a30299978e2e444251565fa7c384
Mark Q <115071580+MarkQwinn@users.noreply.github.com>
2023-06-15 08:59:44 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-sudo apt install unixodbc
+# Создание виртуального окружения
+python3 -m venv myenv
+source myenv/bin/activate
 
+# Установка зависимостей из requirements.txt
+pip install -r requirements.txt
+
+# Установка пакета unixodbc
+sudo apt-get update
+sudo apt-get install unixodbc unixodbc-dev --assume-yes
+
+# Запуск приложения на Python
+python main.py
