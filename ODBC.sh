ca0694ff5c43a9a502e336fe01d2a154020293ad
Mark Q <115071580+MarkQwinn@users.noreply.github.com>
2023-07-11 10:02:51 +0300

    Update ODBC.sh

--- ODBC.sh
+++ ODBC.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-if ! [[ "18.04 20.04 22.04 22.10" == *"$(lsb_release -rs)"* ]];
+if ! [[ "16.04 18.04 20.04 22.04" == *"$(lsb_release -rs)"* ]];
 then
     echo "Ubuntu $(lsb_release -rs) is not currently supported.";
     exit;
@@ -13,10 +12,10 @@ curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list >
 
 exit
 sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
 # optional: for bcp and sqlcmd
-sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
-echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
 # optional: for unixODBC development headers
 sudo apt-get install -y unixodbc-dev
